b45030ead9e9016f48959ab671de56141d8da5f9
juusotaneli <juuso.karvonen@outlook.com>
2019-11-05 19:24:03 +0200

    gradle task no 1

---
+++ viikko2/tehtavat1-4/gradleharjoitus/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import ohtu.Multiplayer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("kuka olet: ");
+        String nimi = scanner.nextLine();
+
+        System.out.println("Hello " + nimi);
+    }
+}
