e4dfbf96f796cb197bdaba014a74f3b04df9fb25
juusotaneli <juuso.karvonen@outlook.com>
2019-11-24 16:50:41 +0200

    sixth task of fourth week

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,80 +1,76 @@
 package ohtu;
 
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
 
-    public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    private int player1_score = 0;
+    private int player2_score = 0;
+    private String player1;
+    private String player2;
+
+    public TennisGame(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName == player1)
+            player1_score += 1;
         else
-            m_score2 += 1;
+            player2_score += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+
+        if (player1_score == player2_score) {
+            return gameIsTied(player1_score);
+        } else if (player1_score >= 4 || player2_score >= 4) {
+            return playerHasWonOrHasAdvantage(player1_score, player2_score);
+        } else {
+            return currentScore(player1_score, player2_score);
+        }    
+            
+    }
+    private String gameIsTied(int score) {
+
+        if (score == 0) {
+            return "Love-All";
+        } else if (score == 1) {
+            return "Fifteen-All";
+        } else if (score == 2) {
+            return "Thirty-All";
+        } else if (score == 3) {
+            return "Forty-All";
+        } else if (score == 4) { 
+            return "Deuce";
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        return "";
+    }
+    private String playerHasWonOrHasAdvantage(int player1_score, int player2_score) {
+
+        if (player1_score - player2_score == 1) {
+            return "Advantage player1";
+        }else if (player1_score - player2_score == -1) {
+            return "Advantage player2";
+        }else if (player1_score - player2_score >= 2) {
+            return "Win for player1";
+        }else {
+            return "Win for player2";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+    }
+    private String currentScore(int player1_score, int player2_score) {
+        return scoreToStringFormat(player1_score) + "-" + scoreToStringFormat(player2_score);
+    }
+    private String scoreToStringFormat (int score) {
+        if (score == 0) {
+            return "Love";
+        }else if (score == 1) {
+            return "Fifteen";
+        }else if (score == 2) {
+            return "Thirty";
+        }else if (score == 3) {
+            return "Forty";
         }
-        return score;
+        return "";
+
     }
 }
